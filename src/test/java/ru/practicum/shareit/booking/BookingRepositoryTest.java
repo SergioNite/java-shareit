0b4678b62eab9f6c6621aeddcb25c9209a65202a
Sergio Nite <nite2008@yandex.ru>
2023-05-20 15:01:10 +0300

    feat: adding tests

---
+++ src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
@@ -0,0 +1,58 @@
+package ru.practicum.shareit.booking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.storage.BookingRepository;
+import ru.practicum.shareit.booking.storage.BookingStatus;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.storage.ItemRepository;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.storage.UserRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+class BookingRepositoryTest {
+    @Autowired
+    private BookingRepository bookingRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ItemRepository itemRepository;
+    private User owner;
+    private User booker;
+    private Booking booking;
+    private Item item;
+    private final LocalDateTime start = LocalDateTime.parse("2023-05-01T01:00");
+    private final LocalDateTime end = LocalDateTime.parse("2023-05-02T01:00");
+
+    @BeforeEach
+    void create() {
+        owner = new User(1L,"owner", "owner@gmail.com");
+        booker = new User(2L,"booker", "boker@gmail.com");
+        item = new Item(1L,"item", "item description", true, owner, null);
+        booking = new Booking(1L,start, end, item, booker, BookingStatus.WAITING);
+        owner = userRepository.save(owner);
+        booker = userRepository.save(booker);
+        item = itemRepository.save(item);
+    }
+
+    @Test
+    void findAllByBookerIdAndStatusOrderByStartDesc() {
+        booking = bookingRepository.save(booking);
+
+        List<Booking> bookings = bookingRepository.findAllByBookerIdAndStatusOrderByStartDesc(
+                booker.getId(), BookingStatus.WAITING, PageRequest.of(0, 2));
+        assertThat(bookings).hasSize(1).contains(booking);
+    }
+
+
+}
---
+++ src/test/java/ru/practicum/shareit/item/ItemRepositoryTest.java
@@ -0,0 +1,49 @@
+package ru.practicum.shareit.item;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.storage.ItemRepository;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.storage.UserRepository;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class ItemRepositoryTest {
+    @Autowired
+    private ItemRepository itemRepository;
+    @Autowired
+    private UserRepository userRepository;
+    private Item item;
+    private User user;
+
+    @BeforeEach
+    void createItem() {
+        user = new User(1L,"User One", "one@gmail.com");
+        item = new Item(1L,"Item One", "item 1", true, user, null);
+    }
+
+    @Test
+    void findByOwner() {
+        User newUser = userRepository.save(user);
+        item.setOwner(newUser);
+        Item newItem = itemRepository.save(item);
+
+        List<Item> items = itemRepository.findAllByOwner(newUser);
+        assertThat(items).hasSize(1).contains(newItem);
+    }
+
+    @Test
+    void findByNameOrDescription() {
+        user = userRepository.save(user);
+        item = itemRepository.save(item);
+
+        List<Item> items = itemRepository.search( "item");
+        assertThat(items).hasSize(1).contains(item);
+    }
+}
